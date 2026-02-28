769b088d2e50c4a9c676456a05d5e3d9f6692eca
Cartman <cartman@xbtlex.dev>
2026-03-01 00:21:19 +1000

    fix: resolve ESLint and JSX syntax errors, add .gitignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,12 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextPlugin from '@eslint/next'
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const eslintConfig = {
+  plugins: {
+    '@next/next': nextPlugin,
+  },
+  rules: {
+    '@next/next/no-html-link-for-pages': 'off',
+  },
+}
 
-export default eslintConfig;
+export default [eslintConfig]
